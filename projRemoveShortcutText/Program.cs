1894c39448633f90df3eadaab3cf5146d54a341c
Christopher Caldwell <ccaldwell488@gmail.com>
2017-09-22 01:17:30 +0100

    initial commit

---
+++ projRemoveShortcutText/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace projRemoveShortcutText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Removes the shortcut text from files such as \"Photoshop - Shortcut\"\nUsage: remShort \"C:\\example_folder\"");
+
+            }
+            else
+            {
+                DirectoryInfo dir = new DirectoryInfo(args[0]);
+
+                foreach (var f in dir.GetFiles())
+                {
+                    if (f.Name.Contains(" - Shortcut"))
+                    {
+                        int start = f.FullName.IndexOf(" - Shortcut");
+                        string newName = f.FullName.Remove(start, 11);
+                        File.Move(f.FullName, newName);
+                    }
+                }
+            }
+        }
+    }
+}
